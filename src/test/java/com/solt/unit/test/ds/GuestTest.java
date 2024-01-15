e1c8e59173fb7de255c6a8b402447675b6049ad0
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-01-15 19:06:53 +0630

    Initial commit

---
+++ src/test/java/com/solt/unit/test/ds/GuestTest.java
@@ -0,0 +1,10 @@
+package com.solt.unit.test.ds;
+
+import java.util.UUID;
+
+public class GuestTest {
+
+    private static final String JOHN="John";
+    private static final String MX7="MX7";
+    private static final UUID GUEST_ID=UUID.randomUUID();
+}
